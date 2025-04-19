c67d1f51e63b2eb80f9af519f25ca5ff1fa6c080
Paul McQuade <paulmcquad@gmail.com>
2025-04-19 21:13:57 +0100

    Books
    Books to Big to compress
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- backup.sh
+++ backup.sh
@@ -4,7 +4,9 @@
 #
 # 00 16 * * * gamer /home/gamer/Backup/backup.sh
 #
+# @reboot gamer /home/gamer/Backup.sh
+#
 # Backup Documents & Books Script
 
-7z a -t7z /home/gamer/Backup/Documents.7z /home/gamer/Documents/ -mx9 -mmt8 -stl
-7z a -t7z /home/gamer/Backup/Books.7z /home/gamer/Books/ -mx9 -mmt8 -stl
+7z a -t7z /home/gamer/Backup/Documents.7z /home/gamer/Documents/ -mx9 -mmt8 -stl;
+#7z a -t7z /home/gamer/Backup/Books.7z /home/gamer/Books/ -mx9 -mmt8 -stl;

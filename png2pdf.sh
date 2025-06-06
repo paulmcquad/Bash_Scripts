4e1f39d67c9b1fe26b499020825dc26a48a12c58
Paul McQuade <paulmcquad@gmail.com>
2025-06-06 21:07:13 +0100

    Magick
    PNG to PDF Script
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ png2pdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+magick L0000.png  L0001.png  L0002.png  L0003.png  L0004.png  L0005.png  L0006.png  L0007.png  L0008.png  L0009.png L0010.png  L0011.png  L0012.png  L0013.png  L0014.png  L0015.png  L0016.png  L0017.png  L0018.png  L0019.png L0020.png U3-1-Radio-Receiver.pdf
